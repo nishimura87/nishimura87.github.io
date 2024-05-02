c4b5569c51a5d7bdf86fb96f17d189a5f4660288
nishimura87 <nishimura.k87@gmail.com>
2024-05-02 17:01:52 +0900

    スクロール修正とinclude

--- js/script.js
+++ js/script.js
@@ -4,24 +4,28 @@ $(function() {
     var $overlay = $('#overlay');
     var $closeModalBtn = $('#closeModalBtn');
 
-    $portfolio.on('click', function() {
+    $portfolio.on('click', function () {
+        $('body').addClass('overflow-hidden'); 
         $modal.removeClass('hidden');
         $overlay.removeClass('hidden');
     });
 
     $modal.on('click', function(e) {
         if (e.target === $modal[0]) {
+            $('body').removeClass('overflow-hidden');
             $modal.addClass('hidden');
             $overlay.addClass('hidden');
         }
     });
 
-    $overlay.on('click', function() {
+    $overlay.on('click', function () {
+        $('body').removeClass('overflow-hidden');
         $modal.addClass('hidden');
         $overlay.addClass('hidden');
     });
 
-    $closeModalBtn.on('click', function() {
+    $closeModalBtn.on('click', function () {
+        $('body').removeClass('overflow-hidden');
         $modal.addClass('hidden');
         $overlay.addClass('hidden');
     });
